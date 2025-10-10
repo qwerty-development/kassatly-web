efb9fbeca8ecf1f2d617834abf212b3bb598d4de
KARL-AJ <karlaj@notqwerty.com>
2025-10-10 15:29:25 +0300

    added propper backgrounds

--- app/freez/page.tsx
+++ app/freez/page.tsx
@@ -8,19 +8,69 @@ import { Footer } from "@/components/layout/Footer";
 // Define the image combinations based on the available files
 const imageCombinations = [
   {
-    name: "black",
-    backgroundImage: "/freez-page/backgrounds/black.png",
-    foregroundImage: "/freez-page/foreground/black.png",
+    name: "apple-grape",
+    backgroundImage: "/freez-page/backgrounds/apple-grape.png",
+    foregroundImage: "/freez-page/foreground/apple-grape-w.png",
   },
   {
-    name: "pink",
-    backgroundImage: "/freez-page/backgrounds/pink.png",
-    foregroundImage: "/freez-page/foreground/pink.png",
+    name: "berry",
+    backgroundImage: "/freez-page/backgrounds/berry.png",
+    foregroundImage: "/freez-page/foreground/berry-m.png",
   },
   {
-    name: "red",
-    backgroundImage: "/freez-page/backgrounds/red.png",
-    foregroundImage: "/freez-page/foreground/red.png",
+    name: "blue-hawaii",
+    backgroundImage: "/freez-page/backgrounds/blue-hawaii.png",
+    foregroundImage: "/freez-page/foreground/blue-hawaii-w.png",
+  },
+  {
+    name: "kiwi-lime",
+    backgroundImage: "/freez-page/backgrounds/kiwi-lime.png",
+    foregroundImage: "/freez-page/foreground/kiwi-lime-w.png",
+  },
+  {
+    name: "lemon-ginger",
+    backgroundImage: "/freez-page/backgrounds/lemon-ginger.png",
+    foregroundImage: "/freez-page/foreground/lemon-ginger-m.png",
+  },
+  {
+    name: "lemon-mint",
+    backgroundImage: "/freez-page/backgrounds/lemon-mint.png",
+    foregroundImage: "/freez-page/foreground/lemon-mint-w.png",
+  },
+  {
+    name: "lychee",
+    backgroundImage: "/freez-page/backgrounds/lychee.png",
+    foregroundImage: "/freez-page/foreground/lychee-m.png",
+  },
+  {
+    name: "mango-peach",
+    backgroundImage: "/freez-page/backgrounds/mango-peach.png",
+    foregroundImage: "/freez-page/foreground/mango-peach-w.png",
+  },
+  {
+    name: "mojito-strawberry",
+    backgroundImage: "/freez-page/backgrounds/mojito-strawberry.png",
+    foregroundImage: "/freez-page/foreground/mojito-strawberry-m.png",
+  },
+  {
+    name: "pineapple-coconut",
+    backgroundImage: "/freez-page/backgrounds/pineapple-coconut.png",
+    foregroundImage: "/freez-page/foreground/pineapple-coconut-m.png",
+  },
+  {
+    name: "pomegranate",
+    backgroundImage: "/freez-page/backgrounds/pomegranate.png",
+    foregroundImage: "/freez-page/foreground/pomegranate-m.png",
+  },
+  {
+    name: "strawberry",
+    backgroundImage: "/freez-page/backgrounds/strawberry.png",
+    foregroundImage: "/freez-page/foreground/strawberry-m.png",
+  },
+  {
+    name: "tamarind",
+    backgroundImage: "/freez-page/backgrounds/tamarind.png",
+    foregroundImage: "/freez-page/foreground/tamarind-m.png",
   },
 ];
 
@@ -62,3 +112,4 @@ export default function FreezPage() {
     </div>
   );
 }
+
